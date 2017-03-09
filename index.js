df11c13b3deba38edbb334bce600cc4865b79d3d
RM <rangers1500@yahoo.com>
2017-03-09 12:55:16 -0800

    Constructor fixes. In Debug.

--- index.js
+++ index.js
@@ -35,18 +35,8 @@
      * Create a TrakerrClient instance
      *
      * @param apiKey API Key for the application
-     * @param url (optional) URL to Trakerr, specify null to use default
      * @param contextAppVersion (optional) application version, defaults to 1.0
      * @param contextEnvName (optional) environment name like "development", "staging", "production" or a custom string
-     * @param contextEnvVersion (optional) environment version
-     * @param contextEnvHostname (optional) environment hostname, defaults to hostname
-     * @param contextAppOS (optional) Operating system
-     * @param contextAppOSVersion (optional)  Operating system version
-     * @param contextAppBrowser (optional) Browser name
-     * @param contextAppBrowserVersion (optional) Browser version
-     * @param contextDataCenter (optional) Data center
-     * @param contextDataCenterRegion (optional) Data center region
-     *
      * @module index
      * @version 1.0.0
      */
@@ -59,20 +49,159 @@
         _this.contextAppVersion = contextAppVersion ? contextAppVersion : '1.0';
         _this.contextEnvName = contextEnvName ? contextEnvName : 'development';
 
-        _this.contextEnvVersion = 'undefined';
-        _this.contextEnvHostname = 'undefined';
-        _this.contextAppOS = 'undefined';
-        _this.contextAppOSVersion = 'undefined';
-        _this.contextAppBrowser = 'undefined';
-        _this.contextAppBrowserVersion = 'undefined';
-        _this.contextDataCenter = 'undefined';
-        _this.contextDataCenterRegion = 'undefined';
 
         if (typeof navigator !== 'undefined') {
-            _this.contextAppOS = navigator.platform;
-            _this.contextAppOSVersion = navigator.oscpu;
-            _this.contextAppBrowser = navigator.appCodeName;
-            _this.contextAppBrowserVersion = navigator.appVersion;
+            //from http://www.javascripter.net/faq/browsern.htm
+            // browser
+            var nVer = navigator.appVersion;
+            var nAgt = navigator.userAgent;
+            var browser = navigator.appName;
+            var version = '' + parseFloat(navigator.appVersion);
+            var majorVersion = parseInt(navigator.appVersion, 10);
+            var nameOffset, verOffset, ix;
+
+            // Opera
+            if ((verOffset = nAgt.indexOf('Opera')) != -1) {
+                browser = 'Opera';
+                version = nAgt.substring(verOffset + 6);
+                if ((verOffset = nAgt.indexOf('Version')) != -1) {
+                    version = nAgt.substring(verOffset + 8);
+                }
+            }
+            // Opera Next
+            if ((verOffset = nAgt.indexOf('OPR')) != -1) {
+                browser = 'Opera';
+                version = nAgt.substring(verOffset + 4);
+            }
+            // Edge
+            else if ((verOffset = nAgt.indexOf('Edge')) != -1) {
+                browser = 'Microsoft Edge';
+                version = nAgt.substring(verOffset + 5);
+            }
+            // MSIE
+            else if ((verOffset = nAgt.indexOf('MSIE')) != -1) {
+                browser = 'Microsoft Internet Explorer';
+                version = nAgt.substring(verOffset + 5);
+            }
+            // Chrome
+            else if ((verOffset = nAgt.indexOf('Chrome')) != -1) {
+                browser = 'Chrome';
+                version = nAgt.substring(verOffset + 7);
+            }
+            // Safari
+            else if ((verOffset = nAgt.indexOf('Safari')) != -1) {
+                browser = 'Safari';
+                version = nAgt.substring(verOffset + 7);
+                if ((verOffset = nAgt.indexOf('Version')) != -1) {
+                    version = nAgt.substring(verOffset + 8);
+                }
+            }
+            // Firefox
+            else if ((verOffset = nAgt.indexOf('Firefox')) != -1) {
+                browser = 'Firefox';
+                version = nAgt.substring(verOffset + 8);
+            }
+            // MSIE 11+
+            else if (nAgt.indexOf('Trident/') != -1) {
+                browser = 'Microsoft Internet Explorer';
+                version = nAgt.substring(nAgt.indexOf('rv:') + 3);
+            }
+            // Other browsers
+            else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) < (verOffset = nAgt.lastIndexOf('/'))) {
+                browser = nAgt.substring(nameOffset, verOffset);
+                version = nAgt.substring(verOffset + 1);
+                if (browser.toLowerCase() == browser.toUpperCase()) {
+                    browser = navigator.appName;
+                }
+            }
+            // trim the version string
+            if ((ix = version.indexOf(';')) != -1) version = version.substring(0, ix);
+            if ((ix = version.indexOf(' ')) != -1) version = version.substring(0, ix);
+            if ((ix = version.indexOf(')')) != -1) version = version.substring(0, ix);
+
+            majorVersion = parseInt('' + version, 10);
+            if (isNaN(majorVersion)) {
+                version = '' + parseFloat(navigator.appVersion);
+                majorVersion = parseInt(navigator.appVersion, 10);
+            }
+
+            // mobile version
+            var mobile = /Mobile|mini|Fennec|Android|iP(ad|od|hone)/.test(nVer);
+
+            // cookie
+            var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+
+            if (typeof navigator.cookieEnabled == 'undefined' && !cookieEnabled) {
+                document.cookie = 'testcookie';
+                cookieEnabled = (document.cookie.indexOf('testcookie') != -1) ? true : false;
+            }
+
+            // system
+            var os = unknown;
+            var clientStrings = [
+                { s: 'Windows 10', r: /(Windows 10.0|Windows NT 10.0)/ },
+                { s: 'Windows 8.1', r: /(Windows 8.1|Windows NT 6.3)/ },
+                { s: 'Windows 8', r: /(Windows 8|Windows NT 6.2)/ },
+                { s: 'Windows 7', r: /(Windows 7|Windows NT 6.1)/ },
+                { s: 'Windows Vista', r: /Windows NT 6.0/ },
+                { s: 'Windows Server 2003', r: /Windows NT 5.2/ },
+                { s: 'Windows XP', r: /(Windows NT 5.1|Windows XP)/ },
+                { s: 'Windows 2000', r: /(Windows NT 5.0|Windows 2000)/ },
+                { s: 'Windows ME', r: /(Win 9x 4.90|Windows ME)/ },
+                { s: 'Windows 98', r: /(Windows 98|Win98)/ },
+                { s: 'Windows 95', r: /(Windows 95|Win95|Windows_95)/ },
+                { s: 'Windows NT 4.0', r: /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/ },
+                { s: 'Windows CE', r: /Windows CE/ },
+                { s: 'Windows 3.11', r: /Win16/ },
+                { s: 'Android', r: /Android/ },
+                { s: 'Open BSD', r: /OpenBSD/ },
+                { s: 'Sun OS', r: /SunOS/ },
+                { s: 'Linux', r: /(Linux|X11)/ },
+                { s: 'iOS', r: /(iPhone|iPad|iPod)/ },
+                { s: 'Mac OS X', r: /Mac OS X/ },
+                { s: 'Mac OS', r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/ },
+                { s: 'QNX', r: /QNX/ },
+                { s: 'UNIX', r: /UNIX/ },
+                { s: 'BeOS', r: /BeOS/ },
+                { s: 'OS/2', r: /OS\/2/ },
+                { s: 'Search Bot', r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/ }
+            ];
+            for (var id in clientStrings) {
+                var cs = clientStrings[id];
+                if (cs.r.test(nAgt)) {
+                    os = cs.s;
+                    break;
+                }
+            }
+
+            var osVersion = unknown;
+
+            if (/Windows/.test(os)) {
+                osVersion = /Windows (.*)/.exec(os)[1];
+                os = 'Windows';
+            }
+
+            switch (os) {
+                case 'Mac OS X':
+                    osVersion = /Mac OS X (10[\.\_\d]+)/.exec(nAgt)[1];
+                    break;
+
+                case 'Android':
+                    osVersion = /Android ([\.\_\d]+)/.exec(nAgt)[1];
+                    break;
+
+                case 'iOS':
+                    osVersion = /OS (\d+)_(\d+)_?(\d+)?/.exec(nVer);
+                    osVersion = osVersion[1] + '.' + osVersion[2] + '.' + (osVersion[3] | 0);
+                    break;
+            }
+
+            _this.contextEnvVersion = navigator.userAgent;
+            _this.contextAppOS = os;
+            _this.contextAppOSVersion = osVersion;
+            _this.contextAppBrowser = browserName;
+            _this.contextAppBrowserVersion = fullVersion;
+
         } else {
             try {
                 var os = require('os');
